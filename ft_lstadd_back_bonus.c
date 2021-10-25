75699d9c836f3aed23ed5cd689793d8188b16643
bayamasa <mapi0213mgt@gmail.com>
2021-10-25 16:46:44 +0900

    rename_new_to_new_list

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,23 +6,23 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 17:30:27 by mhirabay          #+#    #+#             */
-/*   Updated: 2021/10/18 17:30:28 by mhirabay         ###   ########.fr       */
+/*   Updated: 2021/10/25 16:46:08 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_list)
 {
 	t_list	*tmp;
 
-	if (!lst || !new)
+	if (!lst || !new_list)
 		return ;
 	if (*lst == NULL)
-		*lst = new;
+		*lst = new_list;
 	else
 	{
 		tmp = ft_lstlast(*lst);
-		tmp->next = new;
+		tmp->next = new_list;
 	}
 }
--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,16 +6,16 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 17:30:30 by mhirabay          #+#    #+#             */
-/*   Updated: 2021/10/18 17:30:31 by mhirabay         ###   ########.fr       */
+/*   Updated: 2021/10/25 16:46:21 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new_list)
 {	
-	if (!lst || !new)
+	if (!lst || !new_list)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	new_list->next = *lst;
+	*lst = new_list;
 }
